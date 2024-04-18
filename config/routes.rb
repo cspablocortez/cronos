4f012a424e8be496475acc9d71e17848616103b0
Pablo Cortez <57056367+cspablocortez@users.noreply.github.com>
2024-04-17 19:30:00 -0700

    Devise users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+  
   get 'pages/index', to: 'pages#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
